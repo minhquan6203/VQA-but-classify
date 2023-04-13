250ca32b52325175b8d809e078bffba598129602
quan nguyen <21521333@gm.uit.edu.vn>
2023-04-14 00:28:59 +0700

    fix

--- src/train.py
+++ src/train.py
@@ -1,21 +1,31 @@
 import os
 import shutil
 from typing import Dict
-from transformers import TrainingArguments, Trainer, logging
+from transformers import TrainingArguments, Trainer, EarlyStoppingCallback, logging
 
 def setTrainingArgs(config: Dict, device) -> TrainingArguments:
     training_args = config["train"]
     if device.type == 'cuda':
         training_args["fp16"] = True
-        
-    return TrainingArguments(**training_args)
+
+    # Add early stopping callback
+    training_args["load_best_model_at_end"] = True
+    training_args["metric_for_best_model"] = "eval_loss"
+    training_args["greater_is_better"] = False
+    early_stopping_callback = EarlyStoppingCallback(early_stopping_patience=training_args["early_stopping_patience"],
+                                                     early_stopping_threshold=training_args["early_stopping_threshold"])
+
+    return TrainingArguments(**training_args), early_stopping_callback
 
 def trainMultimodalModelForVQA(config, device, dataset, collator, model, compute_metrics):
-    training_args = setTrainingArgs(config, device)
+    training_args, early_stopping_callback = setTrainingArgs(config, device)
     training_args.output_dir = os.path.join(training_args.output_dir, config["model"]["name"])
-    
-    if os.path.isdir(training_args.output_dir):
-        shutil.rmtree(training_args.output_dir)
+    ckpt=sorted(os.listdir(training_args.output_dir))
+    # Load last saved model if exists
+    if os.path.exists(training_args.output_dir):
+        model_checkpoint = os.path.join(training_args.output_dir, ckpt[-1])
+        if os.path.isfile(model_checkpoint):
+            model = model.from_pretrained(model_checkpoint)
 
     multi_trainer = Trainer(
         model,
@@ -23,10 +33,11 @@ def trainMultimodalModelForVQA(config, device, dataset, collator, model, compute
         train_dataset=dataset['train'],
         eval_dataset=dataset['test'],
         data_collator=collator,
-        compute_metrics=compute_metrics
+        compute_metrics=compute_metrics,
+        callbacks=[early_stopping_callback]
     )
 
     train_multi_metrics = multi_trainer.train()
     eval_multi_metrics = multi_trainer.evaluate()
-    
+
     return train_multi_metrics, eval_multi_metrics
