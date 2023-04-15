1b0e857b2ea5bbe29fd56ec6eaae9635a939ad72
quan nguyen <21521333@gm.uit.edu.vn>
2023-04-15 14:10:44 +0700

    fix preprocessing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,8 +4,9 @@ import pathlib
 from numpy.random import RandomState
 import os
 from PIL import Image
+from sklearn.model_selection import train_test_split 
 
-def preprocess(json_file,folder_output):
+def preprocess(json_file,folder_output,img_error):
     os.makedirs(folder_output, exist_ok=True)
     path=folder_output
     # Read data
@@ -14,14 +15,17 @@ def preprocess(json_file,folder_output):
 
     #convert to csv file
     df_image= pd.DataFrame.from_dict(data['images'])
+    
     df_qa = pd.DataFrame.from_dict(data['annotations'])
+    for img in img_error:
+      condition = df_qa['image_id'] == img.replace('.jpg','')
+      df_qa=df_qa.drop(df_qa[condition].index)
     for i in range(len(df_qa['image_id'])):
         df_qa['image_id'][i]=str(df_qa['image_id'][i])
         df_qa['question'][i]=str(df_qa['question'][i])
         if len(df_qa['answer'][i])==0:
           df_qa['answer'][i]='đéo biết'
-    df_qa1=df_qa
-    df_qa1.to_csv(f'{path}/data1.csv',index=False)
+    df_qa.to_csv(f'{path}/data1.csv',index=False)
 
     # answer_space.txt
     p = pathlib.Path(f'{path}/answer_space.txt')
@@ -47,20 +51,23 @@ def preprocess(json_file,folder_output):
     #split file for train, validation and test
     df = pd.read_csv(f'{path}/data1.csv')
     rng = RandomState()
-    train_val = df.sample(frac=0.8, random_state=rng)
-    test = df.loc[~df.index.isin(train_val.index)]
-    train = train_val.sample(frac=0.8889, random_state=rng)
-    val = train_val.loc[~train_val.index.isin(train.index)]
+    df = pd.read_csv(f'{path}/data1.csv')
+    rng = RandomState()
+    train_val, test = train_test_split(df, test_size=0.1, random_state=rng)
+    train, val = train_test_split(train_val, test_size=0.1111, random_state=rng)
+
 
     train.to_csv(f'{path}/train.csv',index=False)
     val.to_csv(f'{path}/val.csv',index=False)
     test.to_csv(f'{path}/test.csv',index=False)
 
 def resize_images(input_folder, output_folder, size):
+    img_error=[]
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     for filename in os.listdir(input_folder):
+      try:
         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
             with Image.open(os.path.join(input_folder, filename)) as im:
                 if im.mode != "RGB":
@@ -68,10 +75,19 @@ def resize_images(input_folder, output_folder, size):
                 im_resized = im.resize(size)
                 output_filename = os.path.join(output_folder, filename)
                 im_resized.save(output_filename)
-
+      except:
+        img_error.append(filename)
+    
+    return img_error
 
 if __name__ == '__main__':
-    json_file = '/content/drive/MyDrive/vivqa_on_book/new_b_all_label.json'
-    folder_output = 'data'
-    preprocess(json_file, folder_output)
-    resize_images('book_cover_img','./data/book_cover_img',(512,512))
+
+    img_error=resize_images('book_fahasa','./data_fahasa/book_fahasa',(512,512))
+    print(len(img_error))
+
+    json_file = '/content/drive/MyDrive/vivqa_on_book/new_b_all_fahasa_label.json'
+    folder_output = 'data_fahasa'
+    preprocess(json_file, folder_output,img_error)
+
+
+
