7cd4b1bb99abac2708cc2aaff828955c4f92ee04
quan nguyen <21521333@gm.uit.edu.vn>
2023-05-15 15:24:07 +0700

    add decoder

---
+++ src/decoder_module/decoder.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from typing import List, Dict, Optional
+from attention_module.attentions import MultiHeadAtt
+from mask.masking import generate_padding_mask, generate_sequential_mask, generate_self_attention_masks, sinusoid_encoding_table
+from utils.positional_feed_forward import PositionWiseFeedForward
+from text_module.text_embedding import Text_Embedding
+from transformers import AutoTokenizer, BertGenerationDecoder
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, config: Dict):
+        super(DecoderLayer, self).__init__()
+
+        self.self_attn = MultiHeadAtt(config)
+        self.enc_attn = MultiHeadAtt(config)
+        self.pwff = PositionWiseFeedForward(config)
+
+    def forward(self, queries, keys, values, self_attention_mask, enc_attention_mask, **kwargs):
+        self_att = self.self_attn(queries, queries, queries, attention_mask=self_attention_mask, **kwargs)
+        enc_att = self.enc_attn(self_att, keys, values, attention_mask=enc_attention_mask, **kwargs)
+
+        ff = self.pwff(enc_att)
+        
+        return ff
+
+
+class Decoder(nn.Module):
+    def __init__(self, config):
+        super(Decoder, self).__init__()
+
+        self.d_model = config['decoder']['d_model']
+        self.N = config['decoder']['layers']
+        self.max_len = config['decoder']['max_len']
+        self.gen = BertGenerationDecoder.from_pretrained(config['decoder']['text_decoder'])
+
+  
+    def forward(self,question: List[str], answer_tokens: torch.Tensor, encoder_features: torch.Tensor, encoder_attention_mask: torch.Tensor):
+        question_token = self.tokenizer(question)
+        fused_input = torch.cat([question_token['input_ids'],encoder_features.unsqueeze(0)],dim=1)
+        out = self.gen(input_ids=fused_input,attention_mask=encoder_attention_mask,labels=answer_tokens)
+        
+        return out.logits
---
+++ src/model/model_gen.py
@@ -0,0 +1,67 @@
+from typing import List, Dict, Optional
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from text_module.text_embedding import Text_Embedding
+from vision_module.vision_embedding import  Vision_Embedding
+from attention_module.attentions import MultiHeadAtt
+from encoder_module.encoder import CoAttentionEncoder
+from decoder_module.decoder import Decoder
+from transformers import AutoTokenizer
+
+#lấy ý tưởng từ MCAN
+class MultimodalVQAModel(nn.Module):
+    def __init__(self,config: Dict, answer_space):
+     
+        super(MultimodalVQAModel, self).__init__()
+        self.num_labels = len(answer_space)
+        self.answer_space = answer_space
+        self.intermediate_dims = config["model"]["intermediate_dims"]
+        self.dropout=config["model"]["dropout"]
+        self.num_attention_heads=config["attention"]['heads']
+        self.d_text = config["text_embedding"]['d_features']
+        self.d_vision = config["vision_embedding"]['d_features']
+        self.text_embbeding = Text_Embedding(config)
+        self.vision_embbeding = Vision_Embedding(config)
+        self.tokenizer = AutoTokenizer.from_pretrained(config["text_embedding"]["text_encoder"])
+        self.decoder = Decoder(config)
+        self.fusion = nn.Sequential(
+            nn.Linear(self.intermediate_dims +self.intermediate_dims, self.intermediate_dims),
+            nn.ReLU(),
+            nn.Dropout(self.dropout),
+        )
+        self.encoder = CoAttentionEncoder(config)
+        self.classifier = nn.Linear(self.intermediate_dims, self.num_labels)
+        self.attention_weights = nn.Linear(self.intermediate_dims, 1)
+        self.criterion = nn.CrossEntropyLoss()
+
+    def forward(self, questions: List[str], images: List[str], labels: Optional[torch.LongTensor] = None):
+        answers = [self.answer_space[label] for label in labels]
+        answers = self.tokenizer(answers)['input_ids']
+        embbed_text, text_mask= self.text_embbeding(questions)
+        embbed_vision, vison_mask = self.vision_embbeding(images)
+        encoded_text, encoded_image = self.encoder(embbed_text, text_mask, embbed_vision, vison_mask)
+        
+        # text_attended = self.attention_weights(torch.tanh(encoded_text))
+        # image_attended = self.attention_weights(torch.tanh(encoded_image))
+        # attention_weights = torch.softmax(torch.cat([text_attended, image_attended], dim=1), dim=1)
+        # attended_text = torch.sum(attention_weights[:, 0].unsqueeze(-1) * encoded_text, dim=1)
+        # attended_image = torch.sum(attention_weights[:, 1].unsqueeze(-1) * encoded_image, dim=1)
+        
+        fused_output = self.fusion(torch.cat([encoded_text, encoded_image], dim=1))
+        fused_mask = self.fusion(torch.cat([text_mask,vison_mask],dim=1))
+        logits = self.decoder(questions,answers,fused_output,fused_mask)
+        logits = F.log_softmax(logits, dim=-1)
+        out = {
+            "logits": logits
+        }
+        if answers is not None:
+            logits=logits.view(-1,105879)
+            labels = labels.view(-1)
+            loss = self.criterion(logits, answers)
+            out["loss"] = loss     
+        return out
+
+def createMultimodalModelForVQA(config: Dict, answer_space: List[str]) -> MultimodalVQAModel:
+    model = MultimodalVQAModel(config, answer_space)
+    return model
