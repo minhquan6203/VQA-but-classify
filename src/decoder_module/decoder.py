ff676dda4d7dcbbbd8050be9f5c075f0d5a7ec24
quan nguyen <21521333@gm.uit.edu.vn>
2023-05-15 20:43:35 +0700

    fix

--- src/decoder_module/decoder.py
+++ src/decoder_module/decoder.py
@@ -3,29 +3,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from typing import List, Dict, Optional
-from attention_module.attentions import MultiHeadAtt
-from mask.masking import generate_padding_mask, generate_sequential_mask, generate_self_attention_masks, sinusoid_encoding_table
-from utils.positional_feed_forward import PositionWiseFeedForward
-from text_module.text_embedding import Text_Embedding
-from transformers import AutoTokenizer, BertGenerationDecoder
-
-
-class DecoderLayer(nn.Module):
-    def __init__(self, config: Dict):
-        super(DecoderLayer, self).__init__()
-
-        self.self_attn = MultiHeadAtt(config)
-        self.enc_attn = MultiHeadAtt(config)
-        self.pwff = PositionWiseFeedForward(config)
-
-    def forward(self, queries, keys, values, self_attention_mask, enc_attention_mask, **kwargs):
-        self_att = self.self_attn(queries, queries, queries, attention_mask=self_attention_mask, **kwargs)
-        enc_att = self.enc_attn(self_att, keys, values, attention_mask=enc_attention_mask, **kwargs)
-
-        ff = self.pwff(enc_att)
-        
-        return ff
-
+from transformers import BertGenerationDecoder
 
 class Decoder(nn.Module):
     def __init__(self, config):
